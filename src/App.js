2a7e30efa3679b06d8ddaa404cef542d995c85f1
haron <haronxlee@gmail.com>
2023-08-14 22:01:04 +0900

    Feat: Router 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Outlet, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import Landing from './pages/Landing';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import ToDo from './pages/ToDo';
 
 function App() {
   return (
@@ -8,8 +11,9 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path='/' element={<Landing />} />
-        <Route path='/signin' element={<Landing />} />
-        <Route path='/signup' element={<Landing />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/todo' element={<ToDo />} />
       </Routes>
     </>
   );
