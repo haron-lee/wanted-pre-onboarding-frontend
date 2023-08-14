16cddc06b39fcc85e2e2a260ee4c1eaa817d7240
haron <haronxlee@gmail.com>
2023-08-14 22:02:54 +0900

    Feat: 랜딩페이지 컴포넌트 생성

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import { SignLayoutStyle } from '../style/Layout';
+import { SLink, Title } from '../style/Common';
 
 const Landing = () => {
   return (
-    <>
-      <h1>랜딩페이지</h1>
-    </>
+    <main>
+      <Layout>
+        <Title>오늘의 할일</Title>
+        <SLink to='/signin'>로그인</SLink>
+        <SLink to='/signup'>회원가입</SLink>
+      </Layout>
+    </main>
   );
 };
 
 export default Landing;
+
+const Layout = styled.div`
+  ${SignLayoutStyle}
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  color: white;
+`;
