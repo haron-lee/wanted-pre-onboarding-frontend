39d41ca1b0968f2631dd157b61bf0c9b3f015a35
haron <haronxlee@gmail.com>
2023-08-14 22:02:13 +0900

    Feat: 재사용 input, button 컴포넌트 생성

--- src/components/common/SignButton.jsx
+++ src/components/common/SignButton.jsx
@@ -1,1 +1,34 @@
-// Button
+import React from 'react';
+import { css, styled } from 'styled-components';
+
+const SignButton = ({ type, children, signupBtn, ...rest }) => {
+  return (
+    <Button
+      type={type ? type : 'button'}
+      data-testid={signupBtn ? 'signup-button' : 'signin-button'}
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default SignButton;
+
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  color: white;
+  background-color: var(--primary);
+  box-sizing: border-box;
+
+  ${(props) =>
+    props.disabled &&
+    css`
+      cursor: default;
+      background-color: var(--secondary);
+      color: #636777;
+    `}
+`;
--- src/components/common/SignInput.jsx
+++ src/components/common/SignInput.jsx
@@ -1,1 +1,41 @@
-// Input
+import React from 'react';
+import { styled } from 'styled-components';
+
+const SignInput = ({ children, type, id, email, ...rest }) => {
+  return (
+    <>
+      <Label htmlFor={id}>{children}</Label>
+      <Input
+        type={type ? type : 'email'}
+        id={id}
+        data-testid={email ? 'email-input' : 'password-input'}
+        {...rest}
+        autoComplete={type === 'password' ? 'off' : 'on'}
+      />
+    </>
+  );
+};
+
+export default SignInput;
+
+const Label = styled.label`
+  display: block;
+  margin-left: 5px;
+  margin-bottom: 10px;
+  color: var(--gray-400);
+  font-size: 13px;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  margin-bottom: ${(props) => (props.$checkError ? '10px' : '20px')};
+  padding: 10px;
+  color: white;
+  background-color: var(--dark-bg);
+  border-radius: 10px;
+
+  &::placeholder {
+    font-size: 11px;
+  }
+`;
