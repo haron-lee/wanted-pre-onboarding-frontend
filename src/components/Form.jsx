6a2ecb29294767be9f22ccadafa7fa030dfa9390
haron <haronxlee@gmail.com>
2023-08-14 22:04:00 +0900

    Feat: 재사용 Form 디자인 및 기능 추가, input 커스텀훅 생성

---
+++ src/components/Form.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import Instance from '../api/Instance';
+import { SignLayoutStyle } from '../style/Layout';
+import { Title, ErrorMSG } from '../style/Common';
+import SignInput from '../components/common/SignInput';
+import SignButton from '../components/common/SignButton';
+import UseInput from '../hooks/useInput';
+
+const Form = ({ isSignUp }) => {
+  const {
+    userInput,
+    pwError,
+    setPwError,
+    emailError,
+    isValid,
+    setIsValid,
+    handleInputChange,
+  } = UseInput();
+  const navigate = useNavigate();
+  const { basicInstance } = Instance();
+
+  const handleSubmit = () => {
+    if (isValid) {
+      // 유효성 검사 통과 시 폼 제출 처리
+      if (isSignUp) {
+        // 회원가입 처리
+        basicInstance
+          .post('auth/signup', userInput)
+          .then((res) => {
+            console.log(res);
+            alert('회원가입에 성공했습니다. 로그인 페이지로 이동합니다.');
+            setIsValid(false);
+            navigate('/signin');
+          })
+          .catch((error) => {
+            console.log(error);
+            setPwError('이미 사용중인 이메일 입니다.');
+          });
+      } else {
+        // 로그인 처리
+        basicInstance
+          .post('auth/signin', userInput)
+          .then((res) => {
+            console.log(res);
+            setIsValid(false);
+            navigate('/todo');
+            localStorage.setItem('token', res.data.access_token);
+          })
+          .catch((error) => {
+            console.log(error);
+            setPwError('이메일 혹은 비밀번호가 맞지 않습니다.');
+          });
+      }
+    }
+  };
+
+  return (
+    <SFormLayout>
+      <Title>{isSignUp ? '회원가입' : '로그인'}</Title>
+      <SignInput
+        id='user-email'
+        email
+        placeholder='이메일을 입력해주세요'
+        name='email'
+        value={userInput.email}
+        onChange={handleInputChange}
+        $checkError={emailError}
+      >
+        이메일
+      </SignInput>
+      {emailError && <ErrorMSG>{emailError}</ErrorMSG>}
+      <SignInput
+        type='password'
+        id='user-pw'
+        placeholder='비밀번호를 입력해주세요'
+        name='password'
+        value={userInput.password}
+        onChange={handleInputChange}
+        $checkError={pwError}
+      >
+        비밀번호
+      </SignInput>
+      {pwError && <ErrorMSG>{pwError}</ErrorMSG>}
+      <SignButton
+        onClick={handleSubmit}
+        signupBtn={isSignUp}
+        disabled={!isValid}
+      >
+        {isSignUp ? '가입하기' : '로그인'}
+      </SignButton>
+    </SFormLayout>
+  );
+};
+
+export default Form;
+
+const SFormLayout = styled.form`
+  ${SignLayoutStyle}
+
+  button {
+    margin-top: 30px;
+  }
+`;
---
+++ src/hooks/useInput.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const UseInput = () => {
+  const [userInput, setUserInput] = useState({
+    email: '',
+    password: '',
+  });
+  const [emailError, setEmailError] = useState('');
+  const [pwError, setPwError] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const emailReg =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      if (!emailReg.test(value)) {
+        setEmailError('올바른 이메일 형식이 아닙니다.');
+      } else {
+        setEmailError('');
+      }
+    } else if (name === 'password') {
+      if (value.length < 8) {
+        setPwError('비밀번호는 8자 이상이어야 합니다.');
+      } else {
+        setPwError('');
+      }
+    }
+    setUserInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return {
+    userInput,
+    emailError,
+    setEmailError,
+    pwError,
+    setPwError,
+    isValid:
+      !emailError && !pwError && !!userInput.email && !!userInput.password,
+    setIsValid,
+    handleInputChange,
+  };
+};
+
+export default UseInput;
