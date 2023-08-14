f6bd852ec1a49cfdfd8f1fa33dfc0308900e4c90
haron <haronxlee@gmail.com>
2023-08-14 22:04:26 +0900

    Feat: Axios 인스턴스 생성

---
+++ src/api/Instance.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+
+const Instance = () => {
+  const URL = 'https://www.pre-onboarding-selection-task.shop/';
+
+  const basicInstance = axios.create({
+    baseURL: URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const authInstance = axios.create({
+    baseURL: URL,
+    headers: {
+      // Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return { basicInstance, authInstance };
+};
+
+export default Instance;
