40a93370eef6c7d633b9df8ffd6a3160ad669d19
haron <haronxlee@gmail.com>
2023-08-15 23:51:58 +0900

    Feat: ToDoList get 하여 컴포넌트에 추가 및 ToDo 컴포넌트 기능 추가

---
+++ src/components/ToDo/ToDoBox.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import ToDoInput from './ToDoInput';
+import NewToDo from './NewToDo';
+import ToDos from '../../api/ToDos';
+
+const ToDoBox = () => {
+  const { getTodo } = ToDos();
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const todos = await getTodo();
+      setData(todos);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const reloadData = async () => {
+    const todos = await getTodo();
+    setData(todos);
+  };
+
+  return isLoading ? (
+    <div>로딩 중...</div>
+  ) : (
+    <main>
+      <ToDoInput onAdded={reloadData} />
+      <ul>
+        {data?.map((todo) => (
+          <NewToDo
+            key={todo.id}
+            text={todo.todo}
+            isCompleted={todo.isCompleted}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default ToDoBox;
--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SLayout } from '../style/Layout';
+import { Title } from '../style/Common';
+import ToDoBox from '../components/ToDo/ToDoBox';
 
 const ToDo = () => {
-  return <div>투두</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/signin');
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      <SLayout>
+        <Title>오늘도 화이팅!</Title>
+        <ToDoBox />
+      </SLayout>
+    </>
+  );
 };
 
 export default ToDo;
