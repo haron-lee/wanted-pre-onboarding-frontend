e7c722405a96c87ac30bdbbb5389e1bc5e5ee576
haron <haronxlee@gmail.com>
2023-08-15 16:51:35 +0900

    Feat: 로그인, 회원가입 리디렉션 기능

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Form from '../components/Form';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/todo');
+    }
+  }, [navigate]);
+
   return <Form isSignUp={true} />;
 };
 
